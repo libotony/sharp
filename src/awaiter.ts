67fa86226b54e0032d444b4d08b9a8c71b66dd10
tony <tony.li@vechain.com>
2019-08-09 17:30:18 +0800

    extract awaiter

---
+++ src/awaiter.ts
@@ -0,0 +1,24 @@
+export interface Awaiter {
+    receipt(txVisitor: Connex.Thor.TransactionVisitor, ticker: Connex.Thor.Ticker): Promise<Connex.Thor.Receipt>
+}
+
+export const Awaiter: Awaiter = {
+    receipt(txVisitor, ticker) {
+        return Promise.race<Promise<Connex.Thor.Receipt>>([
+            (async () => {
+                while (true) {
+                    const receipt = await txVisitor.getReceipt()
+                    if (receipt) {
+                        return receipt
+                    }
+                    await ticker.next()
+                }
+            })(),
+            new Promise((_, reject) => {
+                setTimeout(() => {
+                    reject(new Error(`TX(${txVisitor.id}): no receipt for 20 blocks`))
+                }, 20 * 10 * 1000)
+            }),
+        ])
+    }
+}
